a118e09e93222f097221749856f51a8d2c01c80d
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2020-07-28 12:57:04 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Add your functions here
+
+function map(sourceArray, callback) {
+    let result = []
+    for (let i = 0; i < sourceArray.length; i++) {
+        let element = sourceArray[i]
+        result.push(callback(element))
+    }
+    return result
+}
+
+function reduce(sourceArray, callback, startingPoint) {
+    let total = !!startingPoint ? startingPoint : sourceArray[0]
+    for (let i = !!startingPoint ? 0 : 1; i < sourceArray.length; i++) {
+        total = callback(sourceArray[i], total)
+    }
+    return total
+}
